09c90e7a0e105b6952b1ef5eee45412c2a45cbd0
ElieSol <julie.solacroup@yahoo.fr>
2019-08-15 16:58:34 +0800

    MAJ

--- src/public/react/demo-react/src/App.tsx
+++ src/public/react/demo-react/src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
+import { Route, BrowserRouter as Router, Link } from 'react-router-dom';
 
-import {Button} from 'reactstrap';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
 import {ChartPanel} from './ChartPanel/ChartPanel';
 import {TimelinePlot} from './ChartPanel/TimelinePlot';
+import classnames from 'classnames';
+
+class ImportDisplay extends React.Component{
+  render(){
+    return (
+    <div className="Background-Display">
+          <Button color="primary" className="button" >Import Local Data</Button>
+          <hr style={{color:"blue", height: 2}}/>
+          <Button color="primary" className="button">Import Data from cBioportal</Button>
+    </div>
+    )
+  }
+}
 
 const App: React.FC = () => {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,12 +31,32 @@ const App: React.FC = () => {
           Timeline View
         </p>
       </header>
-      <div className="Background-Display">
-          <Button color="primary" className="button">Import Local Data</Button>
-          <hr style={{color:"blue", height: 2}}/>
-          <Button color="primary" className="button">Import Data from cBioportal</Button>
-      </div>
+      <Nav tabs>
+        <NavItem>
+          <NavLink to="/">
+            Import
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink>
+            Information
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink>
+            Chart Display
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink>
+            Help
+          </NavLink>
+        </NavItem>
+      </Nav>
 
+      <Router>
+        <Route exact path="/" component={ImportDisplay}></Route>
+      </Router>
       
 
       <footer className="App-footer">
